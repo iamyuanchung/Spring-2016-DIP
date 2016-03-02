e9f9d70a14e9f782ad45cc5dc0886e6604910a40
andyyuan <iamyuanchung@gmail.com>
2016-03-02 22:04:33 +0800

    Initial commit of hw1

---
+++ hw1/myREADME.m
@@ -0,0 +1,22 @@
+% DIP Homework Assignment #1
+% March 15, 2016
+% Name: ¡È∑∂¶w
+% ID #: B01902040
+% email: iamyuanchung@gmail.com
+% ###############################################################################
+% Problem 1: Noise Removal
+% Implementation 1: Linear Transformation
+% M-file name: linear_amp.m
+% Usage: linear_amp
+% Output image: linear_amp_lena.256 (or chosen by user)
+% Parameters: windows size = 3
+% Other parameters here
+% ###############################################################################
+disp('Running "linear_amp"...');
+% display some useful information figure(1)
+% create a new figure window
+linear_amp; % invoke your M-file properly!
+disp('Done, "linear_amp", output image is "linear_amp_lena.256"');
+% ###############################################################################
+% Problem 2: Edge Detection
+% ###############################################################################
