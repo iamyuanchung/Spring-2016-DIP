3b2d7dd14c00561b70da40bf35d0352b51a03f6d
andyyuan <iamyuanchung@gmail.com>
2016-03-03 14:39:17 +0800

    start hw1

---
+++ hw1/readraw.m
@@ -0,0 +1,19 @@
+function G = readraw(filename)
+    % readraw - read RAW format gray scale image file from Disk
+    % Usuage  : G = readraw(filename)
+ 
+    disp(['    Retrieving Image ' filename ' ...']);
+      fid = fopen(filename, 'rb');
+    if (fid == - 1)
+        error('can not open imput image filem press CTRL-C to exit \n');
+        pause
+    end
+    pixel = fread(fid, inf, 'uchar');
+    fclose(fid);
+    [Y, X] = size(pixel);
+    Size = (Y * X);
+    N = sqrt(Size);
+    G = zeros(N, N);
+    G(1:Size) = pixel(1:Size);
+    G = G';
+end
---
+++ hw1/writeraw.m
@@ -0,0 +1,17 @@
+function count = writeraw(G, filename)
+    % writeraw - write RAW format gray scale image file to Disk
+    % Usuage   : writeraw(G, filename)
+    % G: image data matrix
+    % filename: file name of the file to write to disk
+    % count: return value, the elements written to file
+ 
+    disp([' Write image data to' filename ' ...']);
+      fid = fopen(filename, 'wb');
+    if (fid == - 1)
+        error('can not open output image filem press CTRL-C to exit \n');
+        pause
+    end
+    G = G';
+    count = fwrite(fid, G, 'uchar');
+    fclose(fid);
+end
