55906a23c717343beb9593514c6c2c2c6075b362
andyyuan <iamyuanchung@gmail.com>
2016-05-20 20:29:51 +0800

    undergoing

--- hw4/shapeAnalysis.m
+++ hw4/shapeAnalysis.m
@@ -43,9 +43,9 @@ for i = 1:n_class
     Cls{end + 1} = data(top:bottom, left:right, i);
 end
 
-for i = 1:n_class
-    imwrite(uint8(Cls{i}), ['./rslt_images/Q1_classes/class', num2str(i), '_trim.png']);
-end
+% for i = 1:n_class
+%     imwrite(uint8(Cls{i}), ['./rslt_images/Q1_classes/class', num2str(i), '_trim.png']);
+% end
 
 mkdir('./rslt_images/', 'Q1_instances/');
 
@@ -57,3 +57,48 @@ n_instance = length(Ins); % n_instance = 22
 %     imwrite(uint8(Ins{i}), ['./rslt_images/Q1_instances/instance', num2str(i), '.png']);
 % end
 
+count = zeros(n_class, 1);
+for i = 1:n_instance
+    curInsSize = size(Ins{i});
+    minDist = inf;
+    for j = 1:n_class
+        curClsSize = size(Cls{j});
+        scaleSize = [max(curInsSize(1), curClsSize(1)) max(curInsSize(2), curClsSize(2))];
+        ClsScaled = imresize(Cls{j}, scaleSize);
+        InsScaled = imresize(Ins{i}, scaleSize);
+        curDist = sum(sum(ClsScaled ~= InsScaled));
+        if (curDist < minDist)
+            minDist = curDist;
+            minIndex = j;
+        end
+    end
+    for j = 1:n_class
+        if (minIndex <= 26) % A ~ Z
+            c = char('A' + minIndex - 1);
+        elseif (minIndex > 26 && minIndex <= 52) % a ~ z
+            c = char('a' + minIndex - 27);
+        elseif (minIndex > 52 && minIndex <= 62) % 0 ~ 9
+            c = char('0' + minIndex - 53);
+        else % ! ~ *
+            c = 'does not occur in Sample1.raw';
+        end
+    end
+    fprintf('sample %d, classify as %s\n', i, c);
+    count(minIndex) = count(minIndex) + 1;
+    % imwrite(uint8(InsScaled), ['/Users/Andy/Desktop/tmp/scale', num2str(i), '.png'])
+end
+
+for i = 1:n_class
+    if (count(i) > 0)
+        if (i <= 26) % A ~ Z
+            c = char('A' + i - 1);
+        elseif (i > 26 && i <= 52) % a ~ z
+            c = char('a' + i - 27);
+        elseif (i > 52 && i <= 62) % 0 ~ 9
+            c = char('0' + i - 53);
+        else % ! ~ *
+            c = 'does not occur in Sample1.raw';
+        end
+        % fprintf('%s: %d\n', c, count(i));    
+    end
+end
